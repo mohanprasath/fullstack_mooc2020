4521fcb2f734e5da59f5a6151aac2ad9ae75ca13
Mohan Prasath Chinnasamy <mohanprasath@users.noreply.github.com>
2020-06-03 14:52:22 +0300

    2.1: course contents step6
    DONE

---
+++ part2/courseinfo/src/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Course from './components/Course'
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <h1>{course.name}</h1>
+      <Course parts={course.parts} /><br />
+    </div>
+      )
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
