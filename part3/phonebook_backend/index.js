99bf2c95bf1aba67252bc893679deeaa77e44a4e
Mohan Prasath Chinnasamy <mohanprasath@users.noreply.github.com>
2020-06-25 18:50:40 +0300

    3.15: Phonebook database, step3
    feat: all CRUD operations completed already in previous exercises. COMPLETED

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -10,27 +10,30 @@ app.use(cors())
 app.use(express.static('build'))
 app.use(express.json())
 
-morgan.token('data', function (req) { 
+  morgan.token('data', function (req) { 
   if(req.method == "POST"){
     return JSON.stringify(req.body);
   }
   return null;
  })
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
+  app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
 
-app.get("/api/persons", (request, response, next) => {
-  Person.find({})
-      .then(result => {
-          response.json(result)
-      })
-      .catch(error => next(error))
-})
+  // get all the persons in the phonebook
+  app.get("/api/persons", (request, response, next) => {
+    Person.find({})
+        .then(result => {
+            response.json(result)
+        })
+        .catch(error => next(error))
+  })
 
+  // display the phonebook when the user opens the url
    app.get('/', (req, res) => {
      res.sendFile("./build/static/index.html")
      // send html automatically
   })
   
+  // Lists the count of all the persons in the phonebook
   app.get('/info', (request, response, next) => {
     Person.find({}).then(result => {
       let info = `<div><p>Phonebook has info for ${result.length} people </p><p>${new Date()}</p></div>`
@@ -40,6 +43,7 @@ app.get("/api/persons", (request, response, next) => {
     
   })
 
+  // Lists a person when an id is provided in the url
   app.get('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
     Person.findById((id)).then(
@@ -51,19 +55,15 @@ app.get("/api/persons", (request, response, next) => {
       }).catch(error => console.log(error))
   })
 
-   app.delete('/api/persons/:id', (request, response) => {
-     const id = request.params.id
-  //   persons = persons.filter(person => person.id !== id)
-        Person.findByIdAndDelete(id).then(() => {
-          response.status(204).end()
-        }).catch(error => console.log(error))
-  //   response.status(204).end()
+  // remove a person from the phonebook when the user chooses the id button
+  app.delete('/api/persons/:id', (request, response) => {
+    const id = request.params.id;
+    Person.findByIdAndDelete(id).then(() => {
+      response.status(204).end();
+    }).catch(error => console.log(error));
   })
 
-  // const generateId = () => {
-  //   return Math.floor(Math.random() * Math.floor(Number.MAX_SAFE_INTEGER))
-  // }
-
+  // add a new person to the phonebook if they have a name and a phone number
   app.post('/api/persons', (request, response, next) => {
     const body = request.body
 
@@ -78,43 +78,33 @@ app.get("/api/persons", (request, response, next) => {
         error: 'Name is missing' 
       })
     }
-  
-    // if(persons.find(person => person.name === body.name)){
-    //   return response.status(400).json({ 
-    //     error: 'Name must be unique' 
-    //   })
-    // }
 
     const person = new Person({
       name: body.name,
       number: body.number
     })
-  
-    // notes = persons.concat(person)
-    // response.json(person)
 
     person.save().then(result =>{
       response.status(204).end()
     }).catch(error => next(error))
   })
 
-app.put("/api/persons/:id", (request, response, next) => {
+  // edit the info of a phonebook entry
+  app.put("/api/persons/:id", (request, response, next) => {
     const id = request.params.id
     const body = request.body
-
     const updatedPerson = {
         name: body.name,
         number: body.number
     }
-
     Person.findByIdAndUpdate(id, updatedPerson, { new: true, runValidators: true, context: "query" })
         .then(result => {
             response.json(result)
         })
         .catch(error => next(error))
-})
+  })
 
-  
+  // listens on a port 
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
   })
