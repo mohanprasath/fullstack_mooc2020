eb544d379edff264990acf1e8fa34b98543624b3
Mohan Prasath Chinnasamy <mohanprasath@users.noreply.github.com>
2020-06-28 13:15:48 +0300

    4.5*: helper functions and unit tests, step3

--- part4/blog_list/utils/list_helper.js
+++ part4/blog_list/utils/list_helper.js
@@ -10,7 +10,15 @@ const totalLikes = (blogs) => {
   return blogs.map(blog => blog.likes).reduce(reducer, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  let likes = blogs.map(blog => blog.likes)
+  let i = likes.indexOf(Math.max(...likes))
+  const result = { title:blogs[i].title, author:blogs[i].author, likes:blogs[i].likes }
+  return result
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
